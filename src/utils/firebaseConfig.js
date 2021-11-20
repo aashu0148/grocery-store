0f485cb24c3e85de271b4f6f267abb75dc138190
aashu <buildforfb@gmail.com>
2021-11-20 10:22:04 +0530

    firebase initalized

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFEBQ0YG2XS6ZjdPtylAjkFBlHJ1v1-fA",
+  authDomain: "grocery0store.firebaseapp.com",
+  projectId: "grocery0store",
+  storageBucket: "grocery0store.appspot.com",
+  messagingSenderId: "1000778407735",
+  appId: "1:1000778407735:web:bb5a2f15d4fb897c6304b2",
+  measurementId: "G-LBGKBCVQR1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
