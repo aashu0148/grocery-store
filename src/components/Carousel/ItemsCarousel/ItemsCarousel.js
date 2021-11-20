77f1d22c0c3be970422ee2ca72cb8cf70afc1c63
aashu <buildforfb@gmail.com>
2021-11-20 09:00:06 +0530

    structure is ready

---
+++ src/components/Carousel/ItemsCarousel/ItemsCarousel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ItemsCarousel() {
+  return <div></div>;
+}
+
+export default ItemsCarousel;
---
+++ src/components/Carousel/MainCarousel/MainCarousel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainCarousel() {
+  return <div></div>;
+}
+
+export default MainCarousel;
