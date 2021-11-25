5bfddb639403eca88ddda89e3f69b162a2b9b96c
aashu <buildforfb@gmail.com>
2021-11-25 08:12:52 +0530

    react-hot-toast- added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import Register from "pages/Merchant/Register/Register";
 import LoginPage from "pages/Merchant/LoginPage/LoginPage";
@@ -9,6 +10,13 @@ import "styles/main.scss";
 function App() {
   return (
     <div className="App">
+      <Toaster
+        toastOptions={{
+          duration: 4000,
+          position: "bottom-right",
+          style: { marginBottom: "30px", marginLeft: "30px" },
+        }}
+      />
       <Router>
         <Routes>
           <Route path="/register" element={<Register />} />
