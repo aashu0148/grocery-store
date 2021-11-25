fcc092c8e21834b4b8b04969e5ec1e2fe87d2927
aashu <buildforfb@gmail.com>
2021-11-25 08:09:36 +0530

    phone verification added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,6 +5,14 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import toast from "react-hot-toast";
+
+import { errorToastLogger, validateMobile } from "./util";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_apiKey,
@@ -19,6 +27,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const storage = getStorage(app);
+
+const auth = getAuth();
+auth.useDeviceLanguage();
 
 export const imageUpload = (
   file,
@@ -60,6 +71,61 @@ export const imageUpload = (
       });
     }
   );
+};
+
+async function fireBaseCaptchVerification() {
+  try {
+    window.recaptchaVerifier = await new RecaptchaVerifier(
+      "recaptcha",
+      { size: "invisible" },
+      auth
+    );
+  } catch (error) {
+    errorToastLogger(
+      error,
+      "Re-captcha verification failed. Refresh the page and try again"
+    );
+    return;
+  }
+}
+
+async function signInWithPhone(mobile) {
+  try {
+    const confirmationResult = await signInWithPhoneNumber(
+      auth,
+      mobile,
+      window.recaptchaVerifier
+    );
+    toast.success("Otp sent successfully");
+    return confirmationResult;
+  } catch (error) {
+    errorToastLogger(error, error.message);
+    return;
+  }
+}
+
+export const sendOtp = async (mobile) => {
+  if (!mobile) return false;
+  if (!validateMobile(mobile)) return false;
+
+  await fireBaseCaptchVerification();
+
+  if (window.recaptchaVerifier) {
+    return await signInWithPhone("+91" + mobile);
+  }
+};
+
+export const verifyOtp = async (confirmationResult, otp) => {
+  try {
+    if (!confirmationResult || !otp) return false;
+    const user = confirmationResult.confirm(otp);
+
+    toast.success("OTP verified successfully");
+    return user;
+  } catch (error) {
+    errorToastLogger(error, "Otp verification failed.");
+    return "";
+  }
 };
 
 export default app;
