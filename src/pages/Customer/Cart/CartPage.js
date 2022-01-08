1d04025633d91a89a76748694e69f314c172c3b6
unknown <dineshsoni8569@gmail.com>
2022-01-08 13:15:14 +0530

    8jan

--- src/pages/Customer/Cart/CartPage.js
+++ src/pages/Customer/Cart/CartPage.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Minus, Plus, X } from "react-feather";
 
-import Button from "components/Button/Button";
+import InputSelect from "components/InputControl/InputSelect/InputSelect";
 
 import styles from "./CartPage.module.scss";
 
@@ -8,96 +9,186 @@ const CartPage = () => {
   return (
     <div className={styles.outer}>
       <div className={styles.leftPortion}>
+        <div className={styles.shopTitle}>
+          Your Shopping Cart <span>(2 Items)</span>
+        </div>
         <div className={styles.leftInner}>
-          <div className={styles.labelTop}>
-            <h4>PRODUCT</h4>
-            <h4>PRICE</h4>
-            <h4>qty</h4>
-            <h4>total</h4>
-          </div>
           <div className={styles.productDetailsbox}>
-            <div className={styles.aboutProducts}>
-              <div className={styles.productInfo}>
-                <div className={styles.productImgbox}>
-                  <img
-                    src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="productName"
-                    className={styles.productImg}
-                  />
+            <div className={styles.productsList}>
+              <div className={styles.productCard}>
+                <div className={styles.productInfo}>
+                  <div className={styles.productImgbox}>
+                    <img
+                      src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="productName"
+                      className={styles.productImg}
+                    />
+                  </div>
+                  <div className={styles.aboutBox}>
+                    <div className={styles.productName}>
+                      Apples Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className={styles.productPrice}>₹ 30.9</div>
+                  </div>
                 </div>
-                <div className={styles.deleteItem}>❌</div>
-                <div className={styles.productName}>
-                  Apples Lorem ipsum dolor sit amet.
+                <div className={styles.availableIn}>
+                  <InputSelect placeholder="package" />
                 </div>
+                <div className={styles.qtyCounter}>
+                  <div className={styles.qtyValue}>1</div>
+                  <div className={styles.qtyOpt}>
+                    <Plus className={styles.qtyInc} />
+                    <Minus className={styles.qtyDec} />
+                  </div>
+                </div>
+                <div className={styles.totalItem_price}>₹ 464.12</div>
               </div>
-              <div className={styles.productPrice}>333.9</div>
-              <div className={styles.qtyCounter}>
-                <div className={styles.qtyDec}>-</div>
-                <div className={styles.qty}>1</div>
-                <div className={styles.qtyInc}>+</div>
-              </div>
-              <div className={styles.totalItem_price}>$464.12</div>
+              <X className={styles.deleteItem} />
             </div>
-            <div className={styles.aboutProducts}>
-              <div className={styles.productInfo}>
-                <div className={styles.productImgbox}>
-                  <img
-                    src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="productName"
-                    className={styles.productImg}
-                  />
+            <div className={styles.productsList}>
+              <div className={styles.productCard}>
+                <div className={styles.productInfo}>
+                  <div className={styles.productImgbox}>
+                    <img
+                      src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="productName"
+                      className={styles.productImg}
+                    />
+                  </div>
+                  <div className={styles.aboutBox}>
+                    <div className={styles.productName}>
+                      Apples Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className={styles.productPrice}>₹ 30.9</div>
+                  </div>
                 </div>
-                <div className={styles.deleteItem}>❌</div>
-                <div className={styles.productName}>Apples</div>
+                <div className={styles.availableIn}>
+                  <InputSelect placeholder="package" />
+                </div>
+                <div className={styles.qtyCounter}>
+                  <div className={styles.qtyValue}>1</div>
+                  <div className={styles.qtyOpt}>
+                    <Plus className={styles.qtyInc} />
+                    <Minus className={styles.qtyDec} />
+                  </div>
+                </div>
+                <div className={styles.totalItem_price}>₹ 464.12</div>
               </div>
-              <div className={styles.productPrice}>333.9</div>
-              <div className={styles.qtyCounter}>
-                <button className={styles.qtyDec}>-</button>
-                <div className={styles.qty}>1</div>
-                <button className={styles.qtyInc}>+</button>
-              </div>
-              <div className={styles.totalItem_price}>$464.12</div>
+              <X className={styles.deleteItem} />
             </div>
-            <div className={styles.aboutProducts}>
-              <div className={styles.productInfo}>
-                <div className={styles.productImgbox}>
-                  <img
-                    src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="productName"
-                    className={styles.productImg}
-                  />
+            <div className={styles.productsList}>
+              <div className={styles.productCard}>
+                <div className={styles.productInfo}>
+                  <div className={styles.productImgbox}>
+                    <img
+                      src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="productName"
+                      className={styles.productImg}
+                    />
+                  </div>
+                  <div className={styles.aboutBox}>
+                    <div className={styles.productName}>
+                      Apples Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className={styles.productPrice}>₹ 30.9</div>
+                  </div>
                 </div>
-                <div className={styles.deleteItem}>❌</div>
-                <div className={styles.productName}>Apples</div>
+                <div className={styles.availableIn}>
+                  <InputSelect placeholder="package" />
+                </div>
+                <div className={styles.qtyCounter}>
+                  <div className={styles.qtyValue}>1</div>
+                  <div className={styles.qtyOpt}>
+                    <Plus className={styles.qtyInc} />
+                    <Minus className={styles.qtyDec} />
+                  </div>
+                </div>
+                <div className={styles.totalItem_price}>₹ 464.12</div>
               </div>
-              <div className={styles.productPrice}>333.9</div>
-              <div className={styles.qtyCounter}>
-                <div className={styles.qtyDec}>-</div>
-                <div className={styles.qty}>1</div>
-                <div className={styles.qtyInc}>+</div>
+              <X className={styles.deleteItem} />
+            </div>
+            <div className={styles.productsList}>
+              <div className={styles.productCard}>
+                <div className={styles.productInfo}>
+                  <div className={styles.productImgbox}>
+                    <img
+                      src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="productName"
+                      className={styles.productImg}
+                    />
+                  </div>
+                  <div className={styles.aboutBox}>
+                    <div className={styles.productName}>
+                      Apples Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className={styles.productPrice}>₹ 30.9</div>
+                  </div>
+                </div>
+                <div className={styles.availableIn}>
+                  <InputSelect placeholder="package" />
+                </div>
+                <div className={styles.qtyCounter}>
+                  <div className={styles.qtyValue}>1</div>
+                  <div className={styles.qtyOpt}>
+                    <Plus className={styles.qtyInc} />
+                    <Minus className={styles.qtyDec} />
+                  </div>
+                </div>
+                <div className={styles.totalItem_price}>₹ 464.12</div>
               </div>
-              <div className={styles.totalItem_price}>$464.12</div>
+              <X className={styles.deleteItem} />
+            </div>
+            <div className={styles.productsList}>
+              <div className={styles.productCard}>
+                <div className={styles.productInfo}>
+                  <div className={styles.productImgbox}>
+                    <img
+                      src="https://images.pexels.com/photos/672101/pexels-photo-672101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                      alt="productName"
+                      className={styles.productImg}
+                    />
+                  </div>
+                  <div className={styles.aboutBox}>
+                    <div className={styles.productName}>
+                      Apples Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className={styles.productPrice}>₹ 30.9</div>
+                  </div>
+                </div>
+                <div className={styles.availableIn}>
+                  <InputSelect placeholder="package" />
+                </div>
+                <div className={styles.qtyCounter}>
+                  <div className={styles.qtyValue}>1</div>
+                  <div className={styles.qtyOpt}>
+                    <Plus className={styles.qtyInc} />
+                    <Minus className={styles.qtyDec} />
+                  </div>
+                </div>
+                <div className={styles.totalItem_price}>₹ 464.12</div>
+              </div>
+              <X className={styles.deleteItem} />
             </div>
           </div>
         </div>
       </div>
       <div className={styles.rightPortion}>
+        <div className={styles.orderTitle}>Order summary</div>
         <div className={styles.rightInner}>
-          <div className={styles.horizontalBar}></div>
           <div className={styles.totalPrice_box}>
             <div className={styles.cartTotal}>
-              <div className={styles.cartTotal_text}>Cart total</div>
-              <div className={styles.cartTotal_number}>$123.123</div>
+              <div className={styles.cartTotal_text}>Total Price</div>
+              <div className={styles.cartTotal_number}>₹ 123.123</div>
             </div>
             <div className={styles.totalPrice_bottomtext}>
               Shipping and taxes calculated at checkout
             </div>
           </div>
           <div className={styles.checkoutBox}>
-            <Button bordered >Checkout</Button>
+            <button type="submit" className={styles.checkoutButton}>
+              Checkout
+            </button>
           </div>
-          <div className={styles.horizontalBar}></div>
-
         </div>
       </div>
     </div>
