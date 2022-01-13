b32f80c26fd9a7cf203b91a7a2ff403a27668314
Nitish0007 <0007nitishsharma@gmail.com>
2022-01-13 14:36:08 +0530

    product section half done

--- src/pages/Merchant/MerchantDashboard/MerchantProduct/MerchantProduct.js
+++ src/pages/Merchant/MerchantDashboard/MerchantProduct/MerchantProduct.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { Search } from "react-feather";
+import { Search, ChevronDown, ChevronUp, Edit2, Trash } from "react-feather";
 
 import Button from "components/Button/Button";
 import AddProductModal from "../MerchantProduct/AddProductModal/AddProductModal";
@@ -9,6 +9,7 @@ import styles from "./MerchantProduct.module.scss";
 
 function MerchantProduct() {
   const [showProductModal, setShowProductModal] = useState(false);
+  const [isSortDropdown, setIsSortDropdown] = useState(false);
 
   const searchInputRef = useRef();
 
@@ -64,6 +65,77 @@ function MerchantProduct() {
         "https://firebasestorage.googleapis.com/v0/b/grocery0store.appspot.com/o/categories%2F3-2-vegetable-transparent.png?alt=media&token=c52f835b-d095-4c71-9b7f-5d23d55fe1f7",
     },
   ];
+
+  const allProducts = [
+    {
+      productName: "Tomato",
+      catergory: "Vegitables",
+      price: "30",
+      selling_quantity: "1",
+      selling_unit: "kg",
+      stock_Available: "100",
+      stock_available_In: "kg",
+    },
+    {
+      productName: "Ginger",
+      catergory: "Vegitables",
+      price: "15",
+      selling_quantity: "250",
+      selling_unit: "gram",
+      stock_Available: "10",
+      stock_available_In: "kg",
+    },
+    {
+      productName: "Mango",
+      catergory: "Fruits",
+      price: "25",
+      selling_quantity: "1",
+      selling_unit: "kg",
+      stock_Available: "50",
+      stock_available_In: "kg",
+    },
+    {
+      productName: "milk",
+      catergory: "dairy",
+      price: "30",
+      selling_quantity: "500",
+      selling_unit: "mL",
+      stock_Available: "15",
+      stock_available_In: "litre",
+    },
+  ];
+
+  const orders = [
+    {
+      productName: "tomato",
+      quantity: "1",
+      unit: "kg",
+      price: "100",
+      status: "delivered",
+    },
+    {
+      productName: "apple",
+      quantity: "1",
+      unit: "kg",
+      price: "100",
+      status: "pending",
+    },
+    {
+      productName: "Tea",
+      quantity: "1",
+      unit: "packet",
+      price: "100",
+      status: "delivered",
+    },
+    {
+      productName: "tomato",
+      quantity: "1",
+      unit: "kg",
+      price: "100",
+      status: "cancelled",
+    },
+  ];
+
   return (
     <>
       <div className={styles.container}>
@@ -75,8 +147,8 @@ function MerchantProduct() {
             </Button>
           </div>
           <div className={styles.categories}>
-            {userCategories.map((catergory) => (
-              <div className={styles.category_card}>
+            {userCategories.map((catergory, i) => (
+              <div className={styles.category_card} key={i}>
                 <div className={styles.category_image}>
                   <img src={catergory.img_src} />
                 </div>
@@ -87,29 +159,94 @@ function MerchantProduct() {
         </div>
         <div className={styles.secondChild}>
           <div className={styles.secondChild_header}>
-            <div className={styles.productsAndOrders}>
+            <div className={styles.secondChild_header_left}>
               <p>All Products</p>
               <p>Orders</p>
-              <p>Delivered</p>
-              <p>Canceled</p>
             </div>
-            <form
-              className={styles.searchBox}
-              onSubmit={(event) => {
-                event.preventDefault();
-                // handleSearch();
-              }}
-            >
-              <input
-                ref={searchInputRef}
-                type="text"
-                className="basic-input"
-                placeholder="Search product"
-              />
-              <div className={styles.searchIcon}>
-                <Search />
+            <div className={styles.secondChild_header_right}>
+              <form
+                className={styles.searchBox}
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  // handleSearch();
+                }}
+              >
+                <input
+                  ref={searchInputRef}
+                  type="text"
+                  className="basic-input"
+                  placeholder="Search product"
+                />
+                <div className={styles.searchIcon}>
+                  <Search />
+                </div>
+              </form>
+              <div className={styles.sortByBtn}>
+                <span onClick={() => setIsSortDropdown(!isSortDropdown)}>
+                  Sort by
+                </span>
+                {isSortDropdown ? <ChevronUp /> : <ChevronDown />}
               </div>
-            </form>
+            </div>
+          </div>
+          <div className={styles.userProductsAndOrdersSlider}>
+            <div className={styles.allProductsSection}>
+              <ul>
+                <li className={styles.productsHeader}>
+                  <span>Product</span>
+                  <span>Category</span>
+                  <span>Price</span>
+                  <span>Stock</span>
+                  <span></span>
+                  <span></span>
+                </li>
+                {allProducts.map((product, i) => (
+                  <li className={styles.productDetails} key={i}>
+                    <span>{product.productName}</span>
+                    <span>{product.catergory}</span>
+                    <span>
+                      &#8377;{product.price}
+                      <span className={styles.perUnit}>
+                        {`${product.selling_quantity} ${product.selling_unit}`}
+                      </span>
+                    </span>
+                    <span>{`${product.stock_Available} ${product.stock_available_In}`}</span>
+                    <span className={styles.icons}>
+                      <span>
+                        <Edit2 />
+                      </span>
+                      <span>
+                        <Trash />
+                      </span>
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={styles.ordersSection}>
+              <div className={styles.orderSectionHeader}>
+                <span>Ordered Placed({orders.length})</span>
+                <span>Quantity</span>
+                <span>Price</span>
+                <span>Status</span>
+              </div>
+              <div className={styles.ordersSectionBody}>
+                {orders.map((order, i) => (
+                  <li className={styles.productDetails} key={i}>
+                    <span>{order.productName}</span>
+                    <span>{`${order.quantity} ${order.unit}`}</span>
+                    <span>&#8377; {order.price}</span>
+                    <span>
+                      <span className={`${styles[order.status]}`}>
+                        {order.status}
+                      </span>
+                    </span>
+                  </li>
+                ))}
+              </div>
+            </div>
+            <div className={styles.deliveredProductsSection}></div>
+            <div className={styles.cancelledProductsSection}></div>
           </div>
         </div>
       </div>
